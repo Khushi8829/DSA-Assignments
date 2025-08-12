f4a1d754efffc2d99a0dcbd816d65f7fbe270927
Khushi8829 <kg002006@gmail.com>
2025-08-12 11:49:28 +0530

    Assignment 1 and 2

---
+++ ASSIGNMENT_1/Q1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World" << endl;
+
+    
+    cout << "This is line 1\n";
+    cout << "This is line 2" << endl; 
+
+    
+    cout << "Name:\tKhushi" << endl;
+    cout << "Age: \t19" << endl;
+
+   
+    cout << "This will make a beep sound\a" << endl;
+
+    
+    cout << "123456\rABC" << endl;
+    
+
+    return 0;
+}
